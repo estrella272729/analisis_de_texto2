3a46e7c585b3f5713b1cdc1ff848a6f56f0d213d
estrella272729 <estrella272729@gmail.com>
2025-09-11 10:02:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from textblob import TextBlob
 import re
 from googletrans import Translator
-import streamlit-lottie
+import streamlit_lottie
 
 from streamlit_lottie import st_lottie
 import json
